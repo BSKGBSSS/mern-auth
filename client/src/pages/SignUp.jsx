7df0e0c4acba88cf823def25e14ce555756b46bb
BSKGBSSS <vbandi02@gmail.com>
2023-10-07 12:32:25 +0530

    complete sign upo page ui 2

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -10,12 +10,11 @@ export default function SignUp() {
         < input type = "text" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' />
         <button className ='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
         </form>
-        <div className ="flec gap-2 mt-5">
+        <div className ="flex gap-2 mt-5">
           <p>Have an account?</p>
           <Link to='/sign-in'>
              <span className=' text-blue-500'>Sign in</span>
           </Link>
-        
         </div>
     </div>
   )
