11853de2db81bd00fef0368a8d13aca48c0ff894
BSKGBSSS <vbandi02@gmail.com>
2023-10-23 13:08:46 +0530

    complete profile page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
-import {useSelector} from "react-redux"
+import {useSelector} from "react-redux";
 
 export default function Header() {
-    const {currentUser} = useSelector(state => state.user)
+    const {currentUser} = useSelector((state) => state.user);
   return (
     <div className='bg-slate-200'>
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -19,7 +19,7 @@ export default function Header() {
                 </Link>
                <Link to='/profile'>
                 {currentUser ? (
-                    <img src = {currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+                    <img src = {currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover'/>
                 ):(
                    <li>Sign In</li>
                 )}
--- client/src/redux/User/userSlice.js
+++ client/src/redux/User/userSlice.js
@@ -15,7 +15,7 @@ const userSlice = createSlice({
             state.loading = true;
         },
         signInSuccess: (state, action) => {
-            state.current = action.payload;
+            state.currentUser = action.payload;
             state.loading = false;
             state.error = false;
         },
