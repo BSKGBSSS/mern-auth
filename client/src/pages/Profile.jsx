bd083b0100a934bb5dde535a5da754cd2be09594
BSKGBSSS <vbandi02@gmail.com>
2023-11-04 15:06:26 +0530

    upload image functionality (fix small bug)

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -36,7 +36,7 @@ export default function Profile() {
     <div className= 'p-3 max-w-lg mx-auto'>
      <h1 className = 'text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className = 'flex flex-col gap-4'> <input type="file" ref={fileRef} hidden accept ='image/*' onChange={(e) =>setImage(e.target.files[0])} /> 
-      <img src={currentUser.profilePicture} alt = "profile" className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2' onClick={() => fileRef.current.click()}></img> 
+      <img src={ formData.profilePicture || currentUser.profilePicture} alt = "profile" className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2' onClick={() => fileRef.current.click()}></img> 
       <p className = 'text-sm self-center'>{imageError ? (
         <span className='text-red-700'> Error uploading image (file size must be less than 2MB) </span>) : imagePercent > 0 && imagePercent < 100 ? ( <span className='text-slate-700'> {`Uploading: ' ${imagePercent} % `}</span> ) : imagePercent === 100 ? ( <span className='text-green-700'> Image uploaded successfully</span>) : ' '}</p>
        <input defaultValue = {currentUser.username} type= 'text' id= 'username' placeholder = 'Username' className = "bg-slate-100 rounded-lg p-3"/>
